033ca48fe5d0dea2bf3918fb56ee4d609b93fea9
Mina Shin <minamishin@hotmail.com>
2019-02-20 14:01:25 -0800

    Added new test error

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,8 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
 
 end
+
